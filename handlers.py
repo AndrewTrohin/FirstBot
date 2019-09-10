e4cde4dd390fecff430169afeeb3c4b113380238
AndrewTrohin <AndrewTrohin@yandex.ru>
2019-09-10 18:23:47 +0300

    Add rules to .gitignore, imppove handlers.py for flake8

--- handlers.py
+++ handlers.py
@@ -12,7 +12,7 @@ import logging
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.DEBUG,
-    filename='bot.log'
+    filename='bot.log',
 )
 logger = logging.getLogger(__name__)
 
